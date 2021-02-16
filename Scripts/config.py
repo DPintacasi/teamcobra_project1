56353ffa666df2f1d6059b0efe428637a2e0ac72
DPintacasi <73974414+DPintacasi@users.noreply.github.com>
2021-02-15 19:47:03 -0500

    census data analysis update

--- Scripts/config.py
+++ Scripts/config.py
@@ -1,7 +1,3 @@
-census_key = "17f75dcc092b8b92d6e74846f0d3f20e8f0eca1a"
-<<<<<<< HEAD
-yelp_key = "55K4Bo8-1xPcxAllkcJ2DSbdFfYlrbOQ1RRRLdcGSxEwZJAJw1LrU31wKqa8TGhhv_gkATAlju4oTlzNP4h-_J5iFqbeMBWbb3eJzKYzbIA_maT3_mNKkMMD8fcnYHYx"
-=======
+census_key = "test"
 
-g_key = "AIzaSyAhacFt5eg-YkbP6hQpTJhRdY7N5nPF_Ss"
->>>>>>> 564aa5ca0da6eeacedb4316c47a60cef44027beb
+g_key = "test"
